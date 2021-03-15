51cb4f2d8ca58891fa12ad366a967623dd69402c
dom <dominik.meyer01@sap.com>
2021-03-15 11:09:36 +0100

    Add demo files

---
+++ docker-action/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+FOO=$1
+
+echo $FOO
+
+set -x
+
+ls -la $HOME
+
+ls -la /github
+ls -la /github/workspace
+ls -la /github/workflow
+ls -la /github/home
+ls -la $RUNNER_TEMP
+ls -la $RUNNER_WORKSPACE
+printenv
+
+
+echo ------------
+
+mvn help:effective-settings
+
+echo ------------
+
+set +x
